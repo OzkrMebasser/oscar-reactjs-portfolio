5cbf8e7fa9fdde1097ec1540f8378812c4693062
OzkrMebasser <oscarmorenoestudios@gmail.com>
2022-08-24 15:41:19 -0500

    first steps new portfolio

---
+++ src/pages/Service/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const Services = () => {
+  return (
+    <div className="container">
+    
+    <h1 className="text-center" style={{paddingTop: "20%"}}>
+
+          Services
+        </h1>
+        
+    </div>
+  )
+}
+export default Services;
