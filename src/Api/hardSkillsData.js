5e52bfb4c42dd868eb39bc0481868ced12372b43
OzkrMebasser <oscarmorenoestudios@gmail.com>
2022-09-02 09:42:09 -0500

    adding features skills

--- src/Api/hardSkillsData.js
+++ src/Api/hardSkillsData.js
@@ -1,26 +1,49 @@
-// {
-  
-  
-  
-  
-//   "hardSkills" : [
-//     {
-//       "id": "1",
-//       "image": "https://firebasestorage.googleapis.com/v0/b/ecommerce-proyecto-b850b.appspot.com/o/islamujerescat.jpg?alt=media&token=946f0dc5-a78a-4f81-a216-a71cec362da8",
-//       "title": "Trabajo en equipo",
-//       "progress": "90%",
-//       "description":
-//         "Lorem impsum sdfodfadf dfadfadfa  fdadfff."
+import {ImHtmlFive,} from "react-icons/im";
+import {IoLogoCss3} from "react-icons/io";
+import {SiJavascript} from "react-icons/si";
+import {FaNode} from "react-icons/fa";
+import '../../src/pages/Skills/Skills.css'
+
+export const skills = [
+    {
+      id: 1,
+      icon: <ImHtmlFive/>,
+      title: "HTML-5",
+      progress: 75,
+      description: "HTML (HyperText Markup Language) is the most basic building block of the Web",
+      class: "icon-html5",
+      variant: "success",
+
       
-//     },
-    
-//      { 
-//       "id": "2",
-//       "image": "https://firebasestorage.googleapis.com/v0/b/ecommerce-proyecto-b850b.appspot.com/o/islamujerescat.jpg?alt=media&token=946f0dc5-a78a-4f81-a216-a71cec362da8",
-//       "title": "Facilidad de palabra",
-//       "progress": "90%",
-//       "description":
-//         "Lorem impsum sdfodfadf dfadfadfa  fdadfff."
-//      }
-// ]
-// }
+    },
+    {
+        id: 2,
+        title: "CCS-3",
+        progress: 75,
+        description: "CSS stands for Cascading Style Sheets. CSS describes how HTML elements are to be displayed on screen.",
+        icon: <IoLogoCss3/>,
+        class: "icon-Css",
+        variant: "success",
+        
+      },
+      {
+        id: 3,
+        title: "JAVASCRIPT",
+        progress: 60,
+        description: "JavaScript is a dynamic programming language that's used for web development, in web applications, for game development, and lots more.",
+        icon: <SiJavascript/>,
+        class: "icon-Js",
+        variant: "info",
+        
+      },
+      {
+        id: 4,
+        title: "NODE-JS",
+        progress: 55,
+        description: "Disfruta de un dia espectacular ",
+        icon: <FaNode/>,
+        class: "icon-NodeJs",
+        variant: "warning",
+      },
+  ];
+  
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,35 +1,71 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, Fragment } from "react";
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Accordion from 'react-bootstrap/Accordion';
 
+
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+import "./Skills.css";
+import { skills } from "../../Api/hardSkillsData";
 
 const Skills = () => {
 
-  const [hardSkills, setHardSkills] = useState();
-
-  const getApiData = async () => {
-
-    const response = await fetch("https://localhost:8080/src/Api/hardSkillsData.json")
-      .then((response) => response.json());
-  
-    // update the state
-    setHardSkills(response);
-
-  };
-  
-  useEffect(() => {
-    getApiData();
-  }, []);
-
   return (
-    <div className="container">
-    
-  {hardSkills &&
-        hardSkills.map((user) => (
-          <div className="item-container">
-            Id:{user.id} <div className="title">Title:{user.title}</div>
-          </div>
-        ))}
+    <Fragment className="cardGrid containerGrid">
+     <Accordion>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header className="texts">MY HARD SKILLS</Accordion.Header>
         
-    </div>
-  )
-}
+        <Accordion.Body>
+      <h1 className="texts">Here are some of my hard skills</h1>
+      <Row xs={2} md={4} className="g-4 texts ">
+        {skills.map((skill, id) => (
+          <Col >
+            <Card className="cardGrid" >
+              <span className={skill.class} >{skill.icon}</span>
+              <Card.Body>
+                <Card.Title>{skill.title}</Card.Title>
+                <Card.Text>{skill.category}</Card.Text>
+                <Card.Text className="textM">{skill.description}</Card.Text>
+                <strong><p className="texts">Aproximate knowledge</p></strong>
+                <ProgressBar className="texts" animated now={skill.progress} label={`${skill.progress}%`} variant={skill.variant} style={{height:"20px"}}/>
+              </Card.Body>
+              
+            </Card>
+          </Col>
+        ))};
+      </Row>
+      </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      </Accordion>
+    </Fragment>
+  );
+};
 export default Skills;
+
+
+  
+  // {skills.map((skill, id) => (
+  //   <div class="card">
+  //     <h3>{skill.title}</h3>
+  //     <i class={skill.icon} aria-hidden="true"></i>
+  //     <p>{skill.category}</p>
+  //     <div class="button">
+  //       <button>Read More</button>
+  //     </div>
+  //   </div>
+  // ))}
