c6bcc9751e651fd67648acc3431ca3d2d4056fe6
OzkrMebasser <oscarmorenoestudios@gmail.com>
2023-10-04 22:33:44 -0700

    feat: sound onclick

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect} from "react";
+import SoundClick from '../Click/interface.mp3';
 import { useTranslation } from 'react-i18next';
+
 
 import NavDropdown from "react-bootstrap/NavDropdown";
 
@@ -27,6 +29,22 @@ const Navbar = ({ isScrolling }) => {
   const handleClick = () => setClick(!click);
   const Close = () => setClick(false);
 
+
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    const audio = new Audio(SoundClick);
+    audio.preload = 'auto';
+    audioRef.current = audio;
+  }, []);
+
+  const playSound = () => {
+    const audio = audioRef.current;
+    audio.currentTime = 0; // Reinicia el audio al principio
+    audio.play();
+  };
+  
+
   return (
     <nav className="navbar"
       // className={`navbar ${isScrolling > 120 ? "scrolling" : null}`}
@@ -53,7 +71,11 @@ const Navbar = ({ isScrolling }) => {
               to="/"
               activeclassname="active "
               className="nav-links"
-              onClick={click ? handleClick : null}
+              // onClick={click ? handleClick  : null}
+              onClick={() => {
+                handleClick();
+                playSound();
+              }}
             >
              {t("navbar.home")}
             </NavLink>
@@ -64,7 +86,11 @@ const Navbar = ({ isScrolling }) => {
               to="/about"
               activeclassname="active"
               className="nav-links"
-              onClick={click ? handleClick : null}
+              // onClick={click ? handleClick && playSound : null}
+              onClick={() => {
+                handleClick();
+                playSound();
+              }}
             >
               {t("navbar.about")}
             </NavLink>
@@ -75,7 +101,11 @@ const Navbar = ({ isScrolling }) => {
               to="/skills"
               activeclassname="active"
               className="nav-links"
-              onClick={click ? handleClick : null}
+              // onClick={click ? handleClick : null}
+              onClick={() => {
+                handleClick();
+                playSound();
+              }}
             >
               {t("navbar.skills")}
             </NavLink>
@@ -87,7 +117,11 @@ const Navbar = ({ isScrolling }) => {
               to="/projects"
               activeclassname="active"
               className="nav-links"
-              onClick={click ? handleClick : null}
+              // onClick={click ? handleClick : null}
+              onClick={() => {
+                handleClick();
+                playSound();
+              }}
             >
                {t("navbar.projects")}
             </NavLink>
@@ -99,7 +133,11 @@ const Navbar = ({ isScrolling }) => {
               to="/contact"
               activeclassname="active"
               className="nav-links"
-              onClick={click ? handleClick : null}
+              // onClick={click ? handleClick : null}
+              onClick={() => {
+                handleClick();
+                playSound();
+              }}
             >
               {t("navbar.contact")}
             </NavLink>
@@ -112,10 +150,10 @@ const Navbar = ({ isScrolling }) => {
               activeclassname="active"
               className="nav-links">
             <NavDropdown   title= {t("navbar.language")}>
-              <NavDropdown.Item onClick={()=> i18n.changeLanguage("en")}>
+              <NavDropdown.Item onClick={()=> i18n.changeLanguage("en") && playSound()}>
                ENGLISH <span className="fi fi-ca"></span> <span className="fi fi-us"></span> <span className="fi fi-uk"></span>
               </NavDropdown.Item>
-              <NavDropdown.Item onClick={()=> i18n.changeLanguage("es")}>
+              <NavDropdown.Item onClick={()=> i18n.changeLanguage("es")  && playSound()}>
                ESPAÑOL <span className="fi fi-mx"></span> <span className="fi fi-es"></span>
               </NavDropdown.Item>
             </NavDropdown>
