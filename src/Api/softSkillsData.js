9cb127d7321d8ad5f25c97c9c513b1a7700479ff
OzkrMebasser <oscarmorenoestudios@gmail.com>
2024-01-11 23:39:27 -0800

    feat: Done w/ softSkills I posses

--- src/Api/softSkillsData.js
+++ src/Api/softSkillsData.js
@@ -1,4 +1,3 @@
-
 const mySoftSkills = {
   creativityTitle: "mySoftSkills.creativityTitle",
   creativityDesc: "mySoftSkills.creativityDesc",
@@ -6,113 +5,115 @@ const mySoftSkills = {
   communicationDesc: "mySoftSkills.communicationDesc",
   adaptabilityTitle: "mySoftSkills.adaptabilityTitle",
   adaptabilityDesc: "mySoftSkills.adaptabilityDesc",
-  problemSolvingTitle : "mySoftSkills.problemSolvingTitle",
-  problemSolvingDesc : "mySoftSkills.problemSolvingDesc",
-
-}
+  problemSolvingTitle: "mySoftSkills.problemSolvingTitle",
+  problemSolvingDesc: "mySoftSkills.problemSolvingDesc",
+  teamworkTitle: "mySoftSkills.teamworkTitle",
+  teamworkDesc: "mySoftSkills.teamworkDesc",
+  detailOrientedTitle: "mySoftSkills.detailOrientedTitle",
+  detailOrientedDesc: "mySoftSkills.detailOrientedDesc",
+  criticalThinkingTitle: "mySoftSkills.criticalThinkingTitle",
+  criticalThinkingDesc: "mySoftSkills.criticalThinkingDesc",
+  leadershipTitle: "mySoftSkills.leadershipTitle",
+  leadershipDesc: "mySoftSkills.leadershipDesc",
+  willingToLearnTitle: "mySoftSkills.willingToLearnTitle",
+  willingToLearnDesc: "mySoftSkills.willingToLearnDesc",
+};
 export const softSkills = [
   {
     id: 1,
-    imageFront: 'https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FcreativityFront.png?alt=media&token=3dc3123e-c7f0-428f-9884-dae6204bd6f8',
-    imageBack: 'https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FcreativityBack.png?alt=media&token=547994fa-0251-4d6b-a536-5755d5f7fd88',
+    imageFront:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FcreativityFront.png?alt=media&token=3dc3123e-c7f0-428f-9884-dae6204bd6f8",
+    imageBack:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FcreativityBack.png?alt=media&token=547994fa-0251-4d6b-a536-5755d5f7fd88",
     title: `${mySoftSkills.creativityTitle}`,
     description: `${mySoftSkills.creativityDesc}`,
   },
   {
     id: 2,
-    imageFront: 'https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FcommunicationFront.png?alt=media&token=a8403394-3476-4c11-9114-9156ddb60f3d',
-    imageBack: 'https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FcommunicationBack.png?alt=media&token=8860c3c3-396c-4492-b9d0-20eab6996ac3',
+    imageFront:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FcommunicationFront.png?alt=media&token=a8403394-3476-4c11-9114-9156ddb60f3d",
+    imageBack:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FcommunicationBack.png?alt=media&token=8860c3c3-396c-4492-b9d0-20eab6996ac3",
     title: `${mySoftSkills.communicationTitle}`,
     description: `${mySoftSkills.communicationDesc}`,
   },
   {
     id: 3,
-    imageFront: 'https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FadaptabilityFront.png?alt=media&token=6b3132d2-94c4-4f69-b710-1ab9b32777d9',
-    imageBack: 'https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FadaptabilityBack.png?alt=media&token=4a9fa6c4-ffb8-4a92-95c3-5be7c19cf697',
+    imageFront:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FadaptabilityFront.png?alt=media&token=6b3132d2-94c4-4f69-b710-1ab9b32777d9",
+    imageBack:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FadaptabilityBack.png?alt=media&token=4a9fa6c4-ffb8-4a92-95c3-5be7c19cf697",
     title: `${mySoftSkills.adaptabilityTitle}`,
     description: `${mySoftSkills.adaptabilityDesc}`,
   },
   {
     id: 4,
-    imageFront: 'https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FproblemSolvingFront.png?alt=media&token=f7085693-2910-4c68-a73e-4cca36f25122',
-    imageBack: 'https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FproblemSolvingBack.png?alt=media&token=38cf9d70-807d-47fd-88b6-4369c031bd62',
+    imageFront:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FproblemSolvingFront.png?alt=media&token=f7085693-2910-4c68-a73e-4cca36f25122",
+    imageBack:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FproblemSolvingBack.png?alt=media&token=38cf9d70-807d-47fd-88b6-4369c031bd62",
     title: `${mySoftSkills.problemSolvingTitle}`,
     description: `${mySoftSkills.problemSolvingDesc}`,
   },
   {
     id: 5,
-    imageFront: 'https://s25.postimg.cc/xyz2/cta-6.png',
-    imageBack: 'https://s25.postimg.cc/xyz2/cta-6.png',
-    title: 'Team Collaboration',
-    description: 'Me destaco en el trabajo en equipo, colaborando de manera efectiva para lograr objetivos comunes. Valoro la diversidad de habilidades y perspectivas en el equipo.',
+    imageFront:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FteamworkFront.png?alt=media&token=9732ff42-8ba1-46dc-bc9f-ca3b8363fd2b",
+    imageBack:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FteamworkBack.png?alt=media&token=7a52be00-5ef6-4198-a9c8-791baba74293",
+    title: `${mySoftSkills.teamworkTitle}`,
+    description: `${mySoftSkills.teamworkDesc}`,
   },
   {
     id: 6,
-    imageFront: 'https://s25.postimg.cc/xyz3/cta-7.png',
-    imageBack: 'https://s25.postimg.cc/xyz3/cta-7.png',
-    title: 'Problem Solving',
-    description: 'Abordo desafíos con una mentalidad analítica, evaluando situaciones críticamente y encontrando soluciones efectivas.',
+    imageFront:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FdetailOrientedFront.png?alt=media&token=d9c2af08-71e6-434c-911a-03aa908c88f7",
+    imageBack:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FdetailOrientedBack.png?alt=media&token=1d4a7f77-2137-4a97-a314-37a827f2a102",
+    title: `${mySoftSkills.detailOrientedTitle}`,
+    description: `${mySoftSkills.detailOrientedDesc}`,
   },
   {
     id: 7,
-    imageFront: 'https://s25.postimg.cc/xyz4/cta-8.png',
-    imageBack: 'https://s25.postimg.cc/xyz4/cta-8.png',
-    title: 'Effective Communication',
-    description: 'Soy un comunicador efectivo, capaz de transmitir información de manera clara y comprensible. Me adapto a la audiencia para una comunicación más efectiva.',
+    imageFront:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FcriticalThinkingFront.png?alt=media&token=326d61c4-ada1-4d7e-9a6a-5aa522a6c401",
+    imageBack:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FcriticalThinkingBack.png?alt=media&token=7721b02d-f04b-4491-87d3-25a9b8e88196",
+    title: `${mySoftSkills.criticalThinkingTitle}`,
+    description: `${mySoftSkills.criticalThinkingDesc}`,
   },
   {
     id: 8,
-    imageFront: 'https://s25.postimg.cc/xyz5/cta-9.png',
-    imageBack: 'https://s25.postimg.cc/xyz5/cta-9.png',
-    title: 'Adaptability',
-    description: 'Me adapto fácilmente a cambios y nuevos enfoques. Mi flexibilidad me permite mantener un alto rendimiento en entornos dinámicos.',
+    imageFront:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FleadershipFront.png?alt=media&token=f7665d81-efe4-4d27-a601-d2541067f6b8",
+    imageBack:
+      "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FleadershipBack.png?alt=media&token=d6dfc927-8449-49ce-a907-d5879be08d18",
+    title: `${mySoftSkills.leadershipTitle}`,
+    description: `${mySoftSkills.leadershipDesc}`,
   },
   {
     id: 9,
-    imageFront: 'https://s25.postimg.cc/xyz6/cta-10.png',
-    imageBack: 'https://s25.postimg.cc/xyz6/cta-10.png',
-    title: 'Leadership',
-    description: 'Muestro habilidades de liderazgo al guiar y motivar a otros hacia el logro de objetivos. Promuevo un ambiente colaborativo y positivo.',
+    imageFront: "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FwilltToLearnFront.png?alt=media&token=6211c94d-3e9e-4be3-b29c-b86f779949b0",
+    imageBack: "https://firebasestorage.googleapis.com/v0/b/oscar-moreno-dev.appspot.com/o/oscar-portfolio-imgs%2FSkills%2FsoftSkills%2FwilltToLearnBack.png?alt=media&token=5c94df43-241a-4f34-afd9-9eb45fc88820",
+    title: `${mySoftSkills.willingToLearnTitle}`,
+    description: `${mySoftSkills.willingToLearnDesc}`,
   },
-  {
-    id: 10,
-    imageFront: 'https://s25.postimg.cc/xyz7/cta-11.png',
-    imageBack: 'https://s25.postimg.cc/xyz7/cta-11.png',
-    title: 'Attention to Detail',
-    description: 'Presto atención a los detalles para garantizar la precisión y la calidad en mis tareas. Mi enfoque meticuloso contribuye a resultados confiables.',
-  },
-  {
-    id: 11,
-    imageFront: 'https://s25.postimg.cc/xyz8/cta-12.png',
-    imageBack: 'https://s25.postimg.cc/xyz8/cta-12.png',
-    title: 'Creativity',
-    description: 'Soy creativo y genero ideas innovadoras para abordar desafíos. Mi mente abierta y creativa aporta un enfoque único a cada proyecto.',
-  },
-  {
-    id: 12,
-    imageFront: 'https://s25.postimg.cc/xyz9/cta-13.png',
-    imageBack: 'https://s25.postimg.cc/xyz9/cta-13.png',
-    title: 'Communication',
-    description: 'Soy un comunicador efectivo, capaz de expresar ideas de manera clara y comprensible. Mi habilidad para transmitir información contribuye a la comprensión mutua.',
-  },
-]
+];
 
-  // {
-  //   id: 13,
-  //   imageFront: 'https://s25.postimg.cc/xyz10/cta-14.png',
-  //   imageBack: 'https://s25.postimg.cc/xyz10/cta-14.png',
-  //   title: 'Problem Solving',
-  //   description: 'A short sentence describing this callout is.',
-  // },
-  // {
-  //   id: 14,
-  //   imageFront: 'https://s25.postimg.cc/xyz11/cta-15.png',
-  //   imageBack: 'https://s25.postimg.cc/xyz11/cta-15.png',
-  //   title: 'Teamwork',
-  //   description: 'A short sentence describing this callout is.',
-  // }
-  
-
+// {
+//   id: 13,
+//   imageFront: 'https://s25.postimg.cc/xyz10/cta-14.png',
+//   imageBack: 'https://s25.postimg.cc/xyz10/cta-14.png',
+//   title: 'Problem Solving',
+//   description: 'A short sentence describing this callout is.',
+// },
+// {
+//   id: 14,
+//   imageFront: 'https://s25.postimg.cc/xyz11/cta-15.png',
+//   imageBack: 'https://s25.postimg.cc/xyz11/cta-15.png',
+//   title: 'Teamwork',
+//   description: 'A short sentence describing this callout is.',
+// }
 
 // export const skillsFE = [
 //   {
